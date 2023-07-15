98f658f2ff58642bb6699d79173e4e3974662d57
bugobliterator <siddharthbharatpurohit@gmail.com>
2023-07-15 11:28:46 +1000

    AP_Periph: add support for configuration as a base

--- AP_Periph/GPS_Base.cpp
+++ AP_Periph/GPS_Base.cpp
@@ -16,16 +16,42 @@
 #include <AP_GPS/AP_GPS.h>
 #include <AP_Filesystem/AP_Filesystem.h>
 #include <stdio.h>
+#include <AP_GPS/AP_GPS_UBLOX.h>
 
 #ifdef ENABLE_BASE_MODE
+
+#if 0
+#define Debug(fmt, args...) can_printf(fmt, ##args)
+#else
+#define Debug(fmt, args...)
+#endif
 
 #define LOCK_ID 0x4c4f434b // "LOCK"
 #define UBX_PREAMBLE1 0xb5
 #define UBX_PREAMBLE2 0x62 
 
+#define CLASS_ACK 0x05
 #define CLASS_RXM 0x02
-#define MSG_RXM_RAW  0x10
+#define CLASS_CFG 0x06
+#define CLASS_MON 0x0a
+#define CLASS_NAV 0x01
+
+#define MSG_ACK_NAK 0x00
+#define MSG_ACK_ACK 0x01
+
+#define MSG_CFG_MSG          0x01
+#define MSG_CFG_RATE         0x08
+#define MSG_CFG_CFG          0x09
+#define MSG_CFG_NAV_SETTINGS 0x24
+#define MSG_CFG_PRT          0x00
+
 #define MSG_RXM_RAWX 0x15
+#define MSG_RXM_SFRBX 0x13
+
+#define MSG_MON_VER 0x04
+
+#define MSG_NAV_SVIN 0x3b
+#define MSG_NAV_PVT 0x07
 
 extern const AP_HAL::HAL& hal;
 
@@ -43,40 +69,46 @@ const AP_Param::GroupInfo GPS_Base::var_info[] = {
     // @User: Standard
     AP_GROUPINFO("_LOGGING",  2, GPS_Base, _logging, 1),
 
+    // @Param: S_IN_ENABLE
+    // @DisplayName: Enable Survey in
+    // @Description: Enable Survey in
+    // @User: Standard
+    AP_GROUPINFO("_S_IN_EN",  3, GPS_Base, _s_in_enabled, 1),
+
     // @Param: S_IN_TIME
     // @DisplayName: Time to wait for Survey in to be asserted
     // @Description: Time to wait for Survey in to be asserted
     // @Range: 0 1000
     // @User: Standard
-    AP_GROUPINFO("_S_IN_TIME",  3, GPS_Base, _s_in_time, 20.0),
+    AP_GROUPINFO("_S_IN_TIME",  4, GPS_Base, _s_in_time, 20.0),
 
     // @Param: S_IN_ACC
     // @DisplayName: Accuracy to wait for Survey in to be asserted
     // @Description: Accuracy to wait for Survey in to be asserted
     // @Range: 0 1000
     // @User: Standard
-    AP_GROUPINFO("_S_IN_ACC",  4, GPS_Base, _s_in_acc, 2.0),
+    AP_GROUPINFO("_S_IN_ACC",  5, GPS_Base, _s_in_acc, 2.0),
 
     // @Param: S_IN_LAT
     // @DisplayName: Latitude to wait for Survey in to be asserted
     // @Description: Latitude to wait for Survey in to be asserted
     // @Range: -90 90
     // @User: Standard
-    AP_GROUPINFO("_S_IN_LAT",  5, GPS_Base, _s_in_lat, 0.0),
+    AP_GROUPINFO("_S_IN_LAT",  6, GPS_Base, _s_in_lat, 0.0),
 
     // @Param: S_IN_LON
     // @DisplayName: Longitude to wait for Survey in to be asserted
     // @Description: Longitude to wait for Survey in to be asserted
     // @Range: -180 180
     // @User: Standard
-    AP_GROUPINFO("_S_IN_LON",  6, GPS_Base, _s_in_lon, 0.0),
+    AP_GROUPINFO("_S_IN_LON",  7, GPS_Base, _s_in_lon, 0.0),
 
     // @Param: S_IN_ALT
     // @DisplayName: Altitude to wait for Survey in to be asserted
     // @Description: Altitude to wait for Survey in to be asserted
     // @Range: -1000 1000
     // @User: Standard
-    AP_GROUPINFO("_S_IN_ALT",  7, GPS_Base, _s_in_alt, -1000.0),
+    AP_GROUPINFO("_S_IN_ALT",  8, GPS_Base, _s_in_alt, -1000.0),
 
     AP_GROUPEND
 };
@@ -135,23 +167,6 @@ void GPS_Base::parse_time_ubx() {
     if (gps_buffer.peek(0) == UBX_PREAMBLE1 &&
         gps_buffer.peek(1) == UBX_PREAMBLE2 &&
         gps_buffer.peek(2) == CLASS_RXM) {
-        // we have a UBX RXM-RAW or RXM-RAWX message
-        if (gps_buffer.peek(3) == MSG_RXM_RAW) {
-            int32_t tow;
-            int16_t week;
-            // peek 4 bytes at offset 6 to get tow
-            for (uint8_t i=0; i<4; i++) {
-                tmp[i] = gps_buffer.peek(6+i);
-            }
-            memcpy(&tow, tmp, 4);
-            // peek 2 bytes at offset 10 to get week
-            tmp[0] = gps_buffer.peek(10);
-            tmp[1] = gps_buffer.peek(11);
-            memcpy(&week, tmp, 2);
-            if (week > 0 && tow >= 0) {
-                gps_week_time((uint16_t)week, (uint32_t)tow);
-            }
-        }
         if (gps_buffer.peek(3) == MSG_RXM_RAWX) {
             double tow;
             int16_t week;
@@ -173,6 +188,352 @@ void GPS_Base::parse_time_ubx() {
     }
 }
 
+void GPS_Base::_update_checksum(uint8_t *data, uint16_t len, uint8_t &ck_a, uint8_t &ck_b)
+{
+    while (len--) {
+        ck_a += *data;
+        ck_b += ck_a;
+        data++;
+    }
+}
+
+bool GPS_Base::_send_message(uint8_t msg_class, uint8_t msg_id, const void *msg, uint16_t size)
+{
+    if (gps_uart->txspace() < (sizeof(struct ubx_header) + 2 + size)) {
+        return false;
+    }
+    // Debug("GPS: sending message %d %d\n", msg_class, msg_id);
+    struct ubx_header header;
+    uint8_t ck_a=0, ck_b=0;
+    header.preamble1 = UBX_PREAMBLE1;
+    header.preamble2 = UBX_PREAMBLE2;
+    header.msg_class = msg_class;
+    header.msg_id    = msg_id;
+    header.length    = size;
+
+    _update_checksum((uint8_t *)&header.msg_class, sizeof(header)-2, ck_a, ck_b);
+    _update_checksum((uint8_t *)msg, size, ck_a, ck_b);
+
+    gps_uart->write_locked((const uint8_t *)&header, sizeof(header), LOCK_ID);
+    gps_uart->write_locked((const uint8_t *)msg, size, LOCK_ID);
+    gps_uart->write_locked((const uint8_t *)&ck_a, 1, LOCK_ID);
+    gps_uart->write_locked((const uint8_t *)&ck_b, 1, LOCK_ID);
+    return true;
+}
+
+bool GPS_Base::parse_ubx(uint8_t data) {
+reset:
+    switch(_step) {
+
+        // Message preamble detection
+        //
+        // If we fail to match any of the expected bytes, we reset
+        // the state machine and re-consider the failed byte as
+        // the first byte of the preamble.  This improves our
+        // chances of recovering from a mismatch and makes it less
+        // likely that we will be fooled by the preamble appearing
+        // as data in some other message.
+        //
+        case 1:
+            if (UBX_PREAMBLE2 == data) {
+                _step++;
+                break;
+            }
+            _step = 0;
+            FALLTHROUGH;
+        case 0:
+            if(UBX_PREAMBLE1 == data)
+                _step++;
+            break;
+
+        // Message header processing
+        //
+        // We sniff the class and message ID to decide whether we
+        // are going to gather the message bytes or just discard
+        // them.
+        //
+        // We always collect the length so that we can avoid being
+        // fooled by preamble bytes in messages.
+        //
+        case 2:
+            _step++;
+            _class = data;
+            _ck_b = _ck_a = data;                       // reset the checksum accumulators
+            break;
+        case 3:
+            _step++;
+            _ck_b += (_ck_a += data);                   // checksum byte
+            _msg_id = data;
+            break;
+        case 4:
+            _step++;
+            _ck_b += (_ck_a += data);                   // checksum byte
+            _payload_length = data;                     // payload length low byte
+            break;
+        case 5:
+            _step++;
+            _ck_b += (_ck_a += data);                   // checksum byte
+            _payload_length += (uint16_t)(data<<8);
+            if (_payload_length > sizeof(_buffer)) {
+                // assume any payload bigger then what we know about is noise
+                _payload_length = 0;
+                _step = 0;
+                goto reset;
+            }
+            _payload_counter = 0;                       // prepare to receive payload
+            if (_payload_length == 0) {
+                // bypass payload and go straight to checksum
+                _step++;
+            }
+            break;
+
+        // Receive message data
+        //
+        case 6:
+            _ck_b += (_ck_a += data);                   // checksum byte
+            if (_payload_counter < sizeof(_buffer)) {
+                _buffer[_payload_counter] = data;
+            }
+            if (++_payload_counter == _payload_length)
+                _step++;
+            break;
+
+        // Checksum and message processing
+        //
+        case 7:
+            _step++;
+            if (_ck_a != data) {
+                Debug("bad cka %x should be %x", data, _ck_a);
+                _step = 0;
+                goto reset;
+            }
+            break;
+        case 8:
+            _step = 0;
+            if (_ck_b != data) {
+                Debug("bad ckb %x should be %x", data, _ck_b);
+                break;                                                  // bad checksum
+            }
+            return true;
+    }
+    return false;
+}
+
+void GPS_Base::handle_ubx_msg()
+{
+    Debug("GPS: got message 0x%x 0x%x", _class, _msg_id);
+    switch (_class) {
+        case CLASS_ACK:
+            if (_msg_id == MSG_ACK_ACK) {
+                if (ubx_config_state == SETTING_SAVE_CONFIG) {
+                    if (_buffer.ack_ack.msg_class == CLASS_CFG &&
+                        _buffer.ack_ack.msg_id == MSG_CFG_CFG) {
+                        // move to next state
+                        can_printf("GPS_Base: config saved");
+                        ubx_config_state++;
+                    }
+                }
+            }
+            break;
+        case CLASS_MON:
+            if (_msg_id == MSG_MON_VER) {
+                can_printf("GPS_Base: SW Version: %s", _buffer.mon_ver.swVersion);
+                can_printf("GPS_Base: HW Version: %s", _buffer.mon_ver.hwVersion);
+                if (ubx_config_state == WAITING_FOR_VERSION) {
+                    // move to next state
+                    ubx_config_state++;
+                }
+            }
+            break;
+        case CLASS_CFG:
+            switch (_msg_id) {
+                case MSG_CFG_PRT:
+                    if (ubx_config_state == GETTING_PORT_INDEX &&
+                        (_buffer.cfg_prt.portID < ARRAY_SIZE(ubx_cfg_msg_rate_6::rates))) {
+                        Debug("Port ID: %d", _buffer.cfg_prt.portID);
+                        _ublox_port = _buffer.cfg_prt.portID;
+                        ubx_config_state++;
+                    }
+                    break;
+                case MSG_CFG_RATE:
+                    if (ubx_config_state == SETTING_NAV_RATE) {
+                        // check that the rate has been set
+                        if (_buffer.cfg_nav_rate.measure_rate_ms == 1000 &&
+                            _buffer.cfg_nav_rate.nav_rate == 1 &&
+                            _buffer.cfg_nav_rate.timeref == 0) {
+                            // move to next state
+                            ubx_config_state++;
+                        } else {
+                            Debug("NAV RATE Incorrect");
+                            _update_setting = true;
+                        }
+                    }
+                    break;
+                case MSG_CFG_MSG:
+                    if (_buffer.cfg_msg_rate_6.msg_class == curr_msg.msg_class &&
+                        _buffer.cfg_msg_rate_6.msg_id == curr_msg.msg_id &&
+                        _payload_length == sizeof(ubx_cfg_msg_rate_6)) {
+                        // check that the rate has been set
+                        if (_buffer.cfg_msg_rate_6.rates[_ublox_port] == curr_msg.rate) {
+                            // move to next state
+                            Debug("MSG(0x%x, 0x%x) Set RATE: %d", curr_msg.msg_class, curr_msg.msg_id, _buffer.cfg_msg_rate_6.rates[_ublox_port]);
+                            ubx_config_state++;
+                        } else {
+                            Debug("MSG(0x%x, 0x%x) Incorrect RATE: %d", curr_msg.msg_class, curr_msg.msg_id, _buffer.cfg_msg_rate_6.rates[_ublox_port]);
+                            _update_setting = true;
+                        }
+                    }
+                    break;
+                default:
+                    break;
+            }
+        default:
+            break;
+    }
+}
+
+bool GPS_Base::configure_message_rate(uint8_t msg_class, uint8_t msg_id, uint8_t rate)
+{
+    if (!_update_setting) {
+        struct ubx_cfg_msg msg;
+        msg.msg_class = msg_class;
+        msg.msg_id = msg_id;
+        curr_msg.msg_class = msg_class;
+        curr_msg.msg_id = msg_id;
+        curr_msg.rate = rate;
+        return _send_message(CLASS_CFG, MSG_CFG_MSG, &msg, sizeof(msg));
+    } else {
+        curr_msg.msg_class = msg_class;
+        curr_msg.msg_id = msg_id;
+        curr_msg.rate = rate;
+        Debug("MSG(0x%x, 0x%x) Setting RATE: %d", curr_msg.msg_class, curr_msg.msg_id, curr_msg.rate);
+        _update_setting = false;
+        return _send_message(CLASS_CFG, MSG_CFG_MSG, &curr_msg, sizeof(curr_msg));
+    }
+}
+
+static uint32_t baudrates[] = { 9600, 19200, 38400, 57600, 115200, 230400, 921600 };
+uint8_t baudrate_index = 0;
+void GPS_Base::do_configurations()
+{
+    // run at 50Hz
+    if (AP_HAL::millis() - _last_config_ms < 20) {
+        return;
+    }
+    // process bytes from GPS
+    while (gps_uart->available_locked(LOCK_ID) > 0) {
+        uint8_t c;
+        if (gps_uart->read_locked(&c, 1, LOCK_ID)) {
+            if (parse_ubx(c)) {
+                handle_ubx_msg();
+            }
+        } else {
+            break;
+        }
+    }
+    _last_config_ms = AP_HAL::millis();
+    if (ubx_config_state == WAITING_FOR_VERSION) {
+        Debug("Trying baudrate %ld", baudrates[baudrate_index]);
+        // try next baudrate
+        gps_uart->begin_locked(baudrates[baudrate_index], 0,0, LOCK_ID);
+        baudrate_index++;
+        baudrate_index %= ARRAY_SIZE(baudrates);
+        ubx_config_state = SETTING_BAUD;
+    }
+    switch (ubx_config_state) {
+        case SETTING_BAUD: {
+            // send the startup blob
+            gps_uart->write_locked((const uint8_t*)UBLOX_SET_BINARY_460800, sizeof(UBLOX_SET_BINARY_460800), LOCK_ID);
+            ubx_config_state++;
+            break;
+        }
+        case CHECKING_VERSION:
+            // set baud rate to 460800
+            Debug("Checking version");
+            gps_uart->begin_locked(460800, 0, 0, LOCK_ID);
+            _send_message(CLASS_MON, MSG_MON_VER, NULL, 0);
+            ubx_config_state++;
+            break;
+        case WAITING_FOR_VERSION:
+            Debug("Waiting for version");
+            break;
+        case GETTING_PORT_INDEX:
+            Debug("Getting port index");
+            _send_message(CLASS_CFG, MSG_CFG_PRT, NULL, 0);
+            break;
+        case SETTING_NAV_RATE:
+            Debug("Setting nav rate");
+            struct ubx_cfg_nav_rate msg;
+            msg.measure_rate_ms = 1000; //1Hz
+            msg.nav_rate = 1;
+            msg.timeref = 0;
+            if (!_update_setting) {
+                _send_message(CLASS_CFG, MSG_CFG_RATE, nullptr, 0);
+            } else {
+                _send_message(CLASS_CFG, MSG_CFG_RATE, (uint8_t*)&msg, sizeof(msg));
+                _update_setting = false;    
+            }
+            break;
+        case SETTING_SURVEY_IN_RATE:
+            Debug("Setting survey in rate");
+            configure_message_rate(CLASS_NAV, MSG_NAV_SVIN, 1);
+            break;
+        case SETTING_PVT_RATE:
+            Debug("Setting PVT rate");
+            configure_message_rate(CLASS_NAV, MSG_NAV_PVT, 1);
+            break;
+        case SETTING_1005_RATE:
+            Debug("Setting 1005 rate");
+            configure_message_rate(0xf5, 0x05, 1);
+            break;
+        case SETTING_1074_RATE:
+            Debug("Setting 1074 rate");
+            configure_message_rate(0xf5, 0x4a, 1);
+            break;
+        case SETTING_1084_RATE:
+            Debug("Setting 1084 rate");
+            configure_message_rate(0xf5, 0x54, 1);
+            break;
+        case SETTING_1094_RATE:
+            Debug("Setting 1094 rate");
+            configure_message_rate(0xf5, 0x5e, 1);
+            break;
+        case SETTING_1124_RATE:
+            Debug("Setting 1124 rate");
+            configure_message_rate(0xf5, 0x7c, 1);
+            break;
+        case SETTING_1230_RATE:
+            Debug("Setting 1230 rate");
+            configure_message_rate(0xf5, 0xe6, 1);
+            break;
+        case SETTING_RXM_RAWX:
+            Debug("Setting RXM_RAWX rate");
+            configure_message_rate(CLASS_RXM, MSG_RXM_RAWX, 1);
+            break;
+        case SETTING_RXM_SFRBX:
+            Debug("Setting RXM_SFRBX rate");
+            configure_message_rate(CLASS_RXM, MSG_RXM_SFRBX, 1);
+            break;
+        case SETTING_SAVE_CONFIG:
+            if ((AP_HAL::millis() - _last_save_config_ms) > 1000) {
+                can_printf("GPS_Base: Saving config");
+                static const ubx_cfg_cfg save_cfg {
+                    clearMask: 0,
+                    saveMask: SAVE_CFG_ALL,
+                    loadMask: 0
+                };
+                _last_save_config_ms = AP_HAL::millis();
+                _send_message(CLASS_CFG, MSG_CFG_CFG, &save_cfg, sizeof(save_cfg));
+            }
+            break;
+        case SETTING_FINISHED:
+            can_printf("GPS_Base: Configuration Finished");
+            _ppk_config_finished = true;
+            break;
+    };
+}
+
+
 void GPS_Base::update() {
     // get GPS port from serial_manager
     if (gps_uart == NULL) {
@@ -188,8 +549,12 @@ void GPS_Base::update() {
 
     // lock the gcs and gps ports
     gps_uart->lock_port(LOCK_ID, LOCK_ID);
-    gcs_uart->lock_port(LOCK_ID, LOCK_ID);
+    if (_s_in_enabled && !_ppk_config_finished) {
+        do_configurations();
+        return;
+    }
 
+    gcs_uart->lock_port(LOCK_ID, LOCK_ID);
     uint8_t byte, last_byte = 0;
 
     if (gcs_uart->get_usb_baud() != gps_uart->get_baud_rate()) {
