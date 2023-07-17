526d2c51004030b1662bb60f5182b370a4d2eb47
bugobliterator <siddharthbharatpurohit@gmail.com>
2023-07-17 14:46:05 +1000

    AP_Periph: update version to 1.8

--- AP_Periph/version.h
+++ AP_Periph/version.h
@@ -1,13 +1,13 @@
 #pragma once
 #include <GCS_MAVLink/GCS_MAVLink.h>
 
-#define THISFIRMWARE "GNSSPeriph V1.0dev"
+#define THISFIRMWARE "GNSSPeriph V1.8"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 1,0,0,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 1
-#define FW_MINOR 3
+#define FW_MINOR 8
 #define FW_PATCH 0
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
 
