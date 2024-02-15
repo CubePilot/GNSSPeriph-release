bf5e50d840238e90cd7caa87a29066450eb8d29b
bugobliterator <siddharthbharatpurohit@gmail.com>
2024-02-15 17:55:18 +1100

    AP_Periph: bump version to 1.13.2

--- AP_Periph/version.h
+++ AP_Periph/version.h
@@ -1,14 +1,14 @@
 #pragma once
 #include <GCS_MAVLink/GCS_MAVLink.h>
 
-#define THISFIRMWARE "GNSSPeriph V1.13.1"
+#define THISFIRMWARE "GNSSPeriph V1.13.2"
 
 // the following line is parsed by the autotest scripts
-#define FIRMWARE_VERSION 1,13,1,FIRMWARE_VERSION_TYPE_DEV
+#define FIRMWARE_VERSION 1,13,2,FIRMWARE_VERSION_TYPE_DEV
 
 #define FW_MAJOR 1
 #define FW_MINOR 13
-#define FW_PATCH 1
+#define FW_PATCH 2
 #define FW_TYPE FIRMWARE_VERSION_TYPE_DEV
 
 
